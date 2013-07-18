50594266f742ad7108b0e387fcef4e2aa60b4d75
Chirag <ccagrawal@gmail.com>
2013-07-17 23:24:03 -0500

    upload to github

---
+++ server.R
@@ -0,0 +1,130 @@
+require(rCharts)
+require(googleVis)
+
+shinyServer(function(input, output) {
+  
+  views <- as.numeric(read.table("viewFile.txt", header = FALSE)[1, 1]) + 1
+  write(views, file = "viewFile.txt")
+  
+  output$count <- renderText({
+    paste("Views:", views, sep = " ")
+  })
+  
+  output$yearChart <- renderChart({
+    sport <- "NBA"
+    year <- input$year
+    
+    yearDataSet <- read.csv(paste("~/ShinyApps/team-ratings/Data/", sport, "_Year.csv", sep = ""), header = TRUE, sep = ",")
+    yearDataSet <- yearDataSet[which(yearDataSet$Year == year), ]
+    sportMin <- floor(min(yearDataSet$Lower.Bound)/2) * 2
+    sportMax <- ceiling(max(yearDataSet$Upper.Bound)/2) * 2
+    
+    if (input$sortTeam == "rating") {
+      yearDataSet <- yearDataSet[order(yearDataSet$Rating, decreasing = TRUE), ]
+    }
+    
+    h1 <- Highcharts$new()
+    h1$chart(borderWidth = 0, height = 680, width = 760)
+    
+    h1$title(text = paste(sport, " Team Ratings: ", year, sep = ""))
+    h1$yAxis(title = list(text = "Adjusted Margin of Victory"), tickInterval = 2, min = sportMin, max = sportMax)
+    
+    h1$tooltip(shared = TRUE)
+    h1$legend(enabled = FALSE)
+    
+    h1$xAxis(categories = as.list(yearDataSet$Team.Name))
+    h1$series(name = "Rating", type = "bar", data = as.list(yearDataSet$Rating), 
+              tooltip = list(pointFormat = "<b>{series.name}:</b> {point.y:.3f}<br>"))
+    
+    intervalWidths <- list()
+    for (team in 1:nrow(yearDataSet)) {
+      intervalWidths[[team]] <- c(yearDataSet[team, 5], yearDataSet[team, 6])
+    }
+    h1$series(name = "Range", type = "errorbar", color = "#0038A8", whiskerLength = "40%", 
+              lineWidth = "1.25", data = intervalWidths, 
+              tooltip = list(pointFormat = "<b>{series.name}:</b> ({point.low: .3f}, {point.high: .3f})"))
+    
+    h1$set(dom = 'yearChart')
+    return(h1)
+  })
+  
+  output$homeChart <- renderChart({
+    sport <- "NBA"
+    
+    homeDataSet <- read.csv(paste("~/ShinyApps/team-ratings/Data/", sport, "_Home.csv", sep = ""), header = TRUE, sep = ",")
+    sportMin <- floor(min(homeDataSet$Lower.Bound)/2) * 2
+    sportMax <- ceiling(max(homeDataSet$Upper.Bound)/2) * 2
+    
+    if (input$sortHome == "rating") {
+      homeDataSet <- homeDataSet[order(homeDataSet$Rating, decreasing = TRUE), ]
+    }
+    
+    h3 <- Highcharts$new()
+    h3$chart(borderWidth = 0, height = 800, width = 760)
+    
+    h3$title(text = paste(sport, " Team Homecourt Advantages", sep = ""))
+    h3$yAxis(title = list(text = "Homecourt Benefit (Points)"), tickInterval = 2, min = sportMin, max = sportMax)
+    
+    h3$tooltip(shared = TRUE)
+    h3$legend(enabled = FALSE)
+    
+    h3$xAxis(categories = as.list(homeDataSet$Team))
+    h3$series(name = "Benefit", type = "bar", data = as.list(homeDataSet$Rating), 
+              tooltip = list(pointFormat = "<b>{series.name}:</b> {point.y:.3f}<br>"))
+    
+    intervalWidths <- list()
+    for (team in 1:nrow(homeDataSet)) {
+      intervalWidths[[team]] <- c(homeDataSet[team, 3], homeDataSet[team, 4])
+    }
+    h3$series(name = "Range", type = "errorbar", color = "#0038A8", whiskerLength = "40%", 
+              lineWidth = "1.1", data = intervalWidths, 
+              tooltip = list(pointFormat = "<b>{series.name}:</b> ({point.low: .3f}, {point.high: .3f})"))
+    
+    h3$set(dom = 'homeChart')
+    return(h3)
+  })
+
+  output$teamChart <- renderChart({
+    sport <- "NBA"
+    selTeams <- input$cbTeams
+    
+    teamDataSet <- read.csv(paste("~/ShinyApps/team-ratings/Data/", sport, "_Team.csv", sep = ""), 
+                            header = TRUE, sep = ",", check.names = FALSE)
+    teamDataSet <- teamDataSet[ , which(names(teamDataSet) %in% selTeams), drop = FALSE]
+    sportMin <- floor(min(teamDataSet)/2) * 2
+    sportMax <- ceiling(max(teamDataSet)/2) * 2
+    
+    h2 <- Highcharts$new()
+    h2$chart(borderWidth = 0, height = 680, width = 760)
+    
+    h2$title(text = paste(sport, " Team Ratings", sep = ""))
+    h2$yAxis(title = list(text = "Adjusted Margin of Victory"), tickInterval = 2, min = sportMin, max = sportMax)
+    
+    h2$tooltip(shared = FALSE)
+    h2$legend(enabled = TRUE)
+    
+    h2$xAxis(categories = seq(1980, 2013, by=1), labels = list(rotation = 60, y = 20), tickPosition = "inside")
+    
+    for (team in 1:ncol(teamDataSet)) {
+      h2$series(name = names(teamDataSet)[team], type = "line", data = as.list(teamDataSet[, team]), 
+                tooltip = list(pointFormat = "<b>{series.name}:</b> {point.y:.3f}<br>"))
+    }
+    
+    h2$set(dom = 'teamChart')
+    return(h2)
+  })
+  
+  output$fullTable <- renderGvis({
+    sport <- "NBA"
+    tableDataSet <- read.csv(paste("~/ShinyApps/team-ratings/Data/", sport, "_Year.csv", sep = ""), header = TRUE, sep = ",")
+    tableDataSet$Lower.Bound <- NULL
+    tableDataSet$Upper.Bound <- NULL
+    tableDataSet$Team.ID <- NULL
+    tableDataSet$Rating <- round(tableDataSet$Rating, digits = 3)
+    tableDataSet$Team.Name <- paste("<center>", tableDataSet$Team.Name, "</center>", sep = "")
+    tableDataSet$Year <- paste("<center>", tableDataSet$Year, "</center>", sep = "")
+    colnames(tableDataSet) <- c("Year", "Team Name", "Rating")
+    t1 <- gvisTable(tableDataSet,options=list(allowHtml = TRUE, width = 450, height = 450))
+    return(t1)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,105 @@
+require(rCharts)
+
+shinyUI(pageWithSidebar(
+  headerPanel("NBA Team Ratings v1.0"),
+  
+  sidebarPanel(
+    conditionalPanel(condition = "input.tab == 1",
+       sliderInput("year", "Year:", 
+                   min=1980, max=2013, value=2013, format="####"),
+       
+       tags$br(),
+       radioButtons("sortTeam", "Sort Method:",
+                    list("Alphabetical" = "alph",
+                         "Rating" = "rating")),
+       tags$br(),
+       tags$p("This app shows NBA Teams' regular season",
+              "ratings (with 95% confidence intervals) for each year.",
+              "The ratings can be interpreted as expected",
+              "margins of victory against an average opponent.",
+              "For example, a team rated +12 should beat the average", 
+              "team by 12 points. A team rated +12 facing a team",
+              "rated -4 should expect to win by 16 points."),
+       
+       tags$br(),
+       tags$p("The rating is determined by strength of opponents",
+              "and margins of victory (or loss) in each game.",
+              "It does not account for homecourt advantage",
+              "or injuries, and it assumes that teams have a constant",
+              "rating for the entire season. This rating scheme",
+              "is simply meant to be a better tool to summarize a season",
+              "than Win % or Average Margin of Victory.")
+    ),
+    
+    conditionalPanel(condition = "input.tab == 2",
+       checkboxGroupInput("cbTeams", "Teams:",
+          c("Atlanta Hawks",
+            "Boston Celtics",
+            "Brooklyn Nets",
+            "Charlotte Bobcats",
+            "Chicago Bulls",
+            "Cleveland Cavaliers",
+            "Dallas Mavericks",
+            "Denver Nuggets",
+            "Detroit Pistons",
+            "Golden State Warriors",
+            "Houston Rockets",
+            "Indiana Pacers",
+            "Los Angeles Clippers",
+            "Los Angeles Lakers",
+            "Memphis Grizzlies",
+            "Miami Heat",
+            "Milwaukee Bucks",
+            "Minnesota Timberwolves",
+            "New Orleans Hornets",
+            "New York Knicks",
+            "Oklahoma City Thunder",
+            "Orlando Magic",
+            "Philadelphia 76ers",
+            "Phoenix Suns",
+            "Portland Trail Blazers",
+            "Sacramento Kings",
+            "San Antonio Spurs",
+            "Toronto Raptors",
+            "Utah Jazz",
+            "Washington Wizards"), selected = c("Chicago Bulls", "Los Angeles Lakers"))
+    ),
+    
+    conditionalPanel(condition = "input.tab == 3",
+       tags$p("Unfortunately, the table takes a few seconds to load.",
+              "If it hasn't shown up yet, please be patient;",
+              "it'll come eventually (I promise). When it does",
+              "come up, click on the headers to sort the column.",
+              "Sorting takes a while too...")
+    ),
+    
+    conditionalPanel(condition = "input.tab == 4",
+                     radioButtons("sortHome", "Sort Method:",
+                                  list("Alphabetical" = "alph",
+                                       "Rating" = "rating")),
+                     tags$br(),
+                     tags$p("Some NBA homecourts are more beneficial than others.",
+                            "Here are NBA teams' regular season homecourt advantages",
+                            "-- measured in points -- over the past 10 years. For",
+                            "example a team with a Homecourt rating of 2.5 means",
+                            "homecourt gives a 2.5 point advantage.")
+    ),
+    
+#     tags$br(),
+#     tags$p("View the",
+#           tags$a(href = "https://github.com/ccagrawal/tennis-sim", "source code"),
+#           "on GitHub."),
+    
+    tags$br(),
+    tags$h5(textOutput("count"))
+  ),
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Chart by Year", showOutput('yearChart', 'highcharts'), value = 1),
+      tabPanel("Chart by Team", showOutput('teamChart', 'highcharts'), value = 2),
+      tabPanel("Sortable Table", htmlOutput('fullTable'), value = 3),
+      tabPanel("Homecourt Advantage", showOutput('homeChart', 'highcharts'), value = 4),
+      id = "tab"
+    )
+  )
+))
